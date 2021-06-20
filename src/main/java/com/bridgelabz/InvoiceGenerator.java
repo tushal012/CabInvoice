eefcdb6d4dd541f932a7e3ae95916330032ff70b
tushal kumar <tushalkashyap11@gmail.com>
2021-06-19 20:48:06 -0700

    Added: functionality to check the fare of multiple ride

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -12,6 +12,14 @@ public class InvoiceGenerator {
         else
             return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides){
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
 }
 
 
---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Ride {
+    public final int time;
+    public  double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}

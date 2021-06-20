fea902638bcc7031c13deb7589099f01d0150db8
tushal kumar <tushalkashyap11@gmail.com>
2021-06-19 20:32:46 -0700

    Functionality to calculate fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the cab Invoice generator");
-    }
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
 
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else
+            return totalFare;
+    }
 }
+
+

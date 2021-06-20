23bb61f55703e030f8a11dafa309716ffdb3215a
tushal kumar <tushalkashyap11@gmail.com>
2021-06-19 20:53:34 -0700

    Added: test case to check for multiple ride

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -23,4 +23,14 @@ public class CabInvoiceTest {
         Assert.assertEquals(5, fare, 0.0);
     }
 
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
+    }
+
 }
